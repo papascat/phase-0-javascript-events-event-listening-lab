41321417d5242c40f748168720badf51b3088c99
catherinegay <papascat@aol.com>
2023-05-06 19:21:59 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+const button = document.getElementById('button');
+
+function addingEventListener(){
+    
+alert('I was clicked!');
+button.addEventListener('click', addingEventListener);
 }
+addingEventListener()
